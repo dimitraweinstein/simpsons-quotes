d94810b9ed4a806de73f5aabd298d4efb08b8b86
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-09-15 12:43:05 -0700

    created a quote component to display fetched quote

---
+++ src/components/quote/Quote.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Quote = ({ image, quote, name }) => {
+  <figure>
+    <img src={image} alt={name} />
+    <figcaption>
+      {quote} - {name}
+    </figcaption>
+  </figure>;
+  
+};
+
+Quote.propTypes = {
+  image: PropTypes.string.isRequired,
+  quote: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+};
+
+export default Quote;

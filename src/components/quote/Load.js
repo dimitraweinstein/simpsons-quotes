2123c03f6b88d2653c43a116fac9d391a7b5c7c3
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-09-15 12:47:26 -0700

    created load compnent for fetch quoate button

---
+++ src/components/quote/Load.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Load = ({ onClick }) => {
+  <button
+    onClick={onClick}
+    aria-label="Load Quote!"
+  >
+    Load Quote!
+  </button>;
+};
+
+Load.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
+
+export default Load;
